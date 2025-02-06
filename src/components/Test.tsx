253b3fdaafc9e400cc94187adb9c52009a7388b3
Kayxue <s0990006@gmail.com>
2025-02-06 16:22:31 +0800

    Format files

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,11 +1,11 @@
 import { createSignal, onCleanup } from "solid-js";
 
-export function Counter(){
-    const [counter,setCounter]=createSignal(0)
+export function Counter() {
+    const [counter, setCounter] = createSignal(0)
     const oddCounter = () => counter() * 2
-    const [timeCount,setTimeCount]=createSignal(0)
+    const [timeCount, setTimeCount] = createSignal(0)
 
-    const timer=setInterval(() => setTimeCount(timeCount()+1),1000)
+    const timer = setInterval(() => setTimeCount(timeCount() + 1), 1000)
     onCleanup(() => clearInterval(timer))
 
     return (
@@ -14,8 +14,8 @@ export function Counter(){
             <p>You have watch this website for {timeCount()} second.</p>
             <p>Counter: {counter()}</p>
             <p>Even Counter: {oddCounter()}</p>
-            <button onClick={() => setCounter(counter()+1)}>+</button>
-            <button onClick={() => setCounter(counter()-1)}>-</button>
+            <button onClick={() => setCounter(counter() + 1)}>+</button>
+            <button onClick={() => setCounter(counter() - 1)}>-</button>
         </>
     )
 }
