16ce1267cdd28887b5b7e1ec4844c727de1d4fae
Kayxue <s0990006@gmail.com>
2025-02-06 16:21:20 +0800

    Add some counters

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,12 +1,19 @@
-import { createSignal } from "solid-js";
+import { createSignal, onCleanup } from "solid-js";
 
 export function Counter(){
     const [counter,setCounter]=createSignal(0)
+    const oddCounter = () => counter() * 2
+    const [timeCount,setTimeCount]=createSignal(0)
+
+    const timer=setInterval(() => setTimeCount(timeCount()+1),1000)
+    onCleanup(() => clearInterval(timer))
 
     return (
         <>
             <h1>SolidJs Counter Test</h1>
-            <p>{counter()}</p>
+            <p>You have watch this website for {timeCount()} second.</p>
+            <p>Counter: {counter()}</p>
+            <p>Even Counter: {oddCounter()}</p>
             <button onClick={() => setCounter(counter()+1)}>+</button>
             <button onClick={() => setCounter(counter()-1)}>-</button>
         </>
